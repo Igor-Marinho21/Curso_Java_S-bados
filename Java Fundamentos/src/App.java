3b75e52db6baf3753f279cffe85b7abdfa5726b6
unknown <igormalta300@gmail.com>
2024-09-21 16:56:15 -0300

    Primeiro arquivo

---
+++ Java Fundamentos/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a, b;
+        a = 10;
+        b = 20;
+        int c = a + b ;
+        System.out.println(c);
+        
+    }
+}
+   
